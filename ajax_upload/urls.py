bd22d2118bd0e075c2237c07126d686067dad373
Boris Chervenkov <boris.chervenkov@sentido.bg>
2014-03-21 03:28:13 +0200

    Deprecation & PEP8 fixes

--- ajax_upload/urls.py
+++ ajax_upload/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
-urlpatterns = patterns('ajax_upload.views',
+urlpatterns = patterns(
+    'ajax_upload.views',
     url(r'^$', 'upload', name='ajax-upload'),
     url(r'^preview/(?P<param>.+)', 'preview', name='preview-url'),
 )
